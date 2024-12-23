9b52c178e54008086bf25cc5819780741549be1f
Anjihee <jihee5100@sookmyung.ac.kr>
2024-12-23 17:37:56 +0900

    Initial Commit

---
+++ GithhubTest/GithhubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithhubTestApp.swift
+//  GithhubTest
+//
+//  Created by 안지희 on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GithhubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithhubTestTests/GithhubTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithhubTestTests.swift
+//  GithhubTestTests
+//
+//  Created by 안지희 on 12/23/24.
+//
+
+import Testing
+@testable import GithhubTest
+
+struct GithhubTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
